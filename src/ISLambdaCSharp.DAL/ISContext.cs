b9ebada3d8f033967c6db9f0b2d290f09016282f
Michael Joye <michaeljoye@live.com>
2018-07-06 11:27:24 -0400

    test project

---
+++ src/ISLambdaCSharp.DAL/ISContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISLambdaCSharp.DAL
+{
+	public class ISContext : DbContext
+	{
+		public DbSet<Book> Book { get; set; }
+
+		public DbSet<Publisher> Publisher { get; set; }
+
+		public DbSet<Sever> Severs { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql("server=localhost;database=innersanctum;user=innersanctum;password=innersanctum;SslMode=none");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Publisher>(entity =>
+			{
+				entity.HasKey(e => e.ID);
+				entity.Property(e => e.Name).IsRequired();
+			});
+
+			modelBuilder.Entity<Book>(entity =>
+			{
+				entity.HasKey(e => e.ISBN);
+				entity.Property(e => e.Title).IsRequired();
+				entity.HasOne(d => d.Publisher)
+				  .WithMany(p => p.Books);
+			});
+			modelBuilder.Entity<Sever>(entity =>
+			{
+				entity.HasKey(e => e.SeverID).HasAnnotation("Column",new { TypeName= "BINARY(16)"});
+				entity.Property(e => e.MaxCapaticy).HasDefaultValue(100);
+			});
+		}
+	}
+}
---
+++ src/ISLambdaCSharp.DAL/test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ISLambdaCSharp.DAL
+{
+	public class Book
+	{
+		public string ISBN { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public string Language { get; set; }
+		public int Pages { get; set; }
+		public virtual Publisher Publisher { get; set; }
+	}
+
+	public class Publisher
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public virtual ICollection<Book> Books { get; set; }
+	}
+
+	public class Sever
+	{
+		[Column(TypeName = "BINARY(16)")]
+		public Guid SeverID { get; set; }
+		public int CurrentCapaticy { get; set; }
+
+		public int MaxCapaticy { get; set; }
+
+	}
+}
